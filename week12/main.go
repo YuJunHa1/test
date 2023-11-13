ba852f156ecf9f544ded4d090eea8c980e636139
playersh <cshcdh123@gmail.com>
2023-11-13 17:31:52 +0900

    week12 v3.1

--- week12/main.go
+++ week12/main.go
@@ -3,30 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{0, 0, 0, 0, 0}
+	a := []string{"a", "b", "c", "d"}
+	as := a[:2]
+	as[1] = "Z"
+	fmt.Println(a, as)
 
-	s[4] = 100
-	s[0] = 7
-	s[1] = 91
-
-	for _, v := range s {
-		fmt.Println(v)
-	}
-
-	copyS := s[1:4]
-	for _, v := range copyS {
-		fmt.Println(v)
-	}
-
-	test := [3]string{"inha", "go", "student"}
-	testS := test[:2]
-	testS2 := test[1:]
-
-	//testS2[0] = "python"
-	//test[1] = "python"
-	testS[1] = "python"
-
-	fmt.Println(test, len(test))
-	fmt.Println(testS, len(testS))
-	fmt.Println(testS2, len(testS2))
+	b := [4]int{4, 3, 2, 1}
+	bs := b[1:3]
+	fmt.Println(b, bs)
 }
